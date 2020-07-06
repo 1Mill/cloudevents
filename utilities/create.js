50fa5860f5a172c6e72a62c7b95efa1316905e9e
1Mill <erik.ekberg@outlook.com>
2020-07-06 15:20:09 -0700

    Move everything over

---
+++ utilities/create.js
@@ -0,0 +1,21 @@
+const create = ({
+	data = null,
+	datacontenttype = "application/json",
+	id,
+	source,
+	specversion = "1.0",
+	type,
+}) => {
+	const cloudevent = {
+		data: JSON.stringify(data),
+		datacontenttype,
+		id,
+		source,
+		specversion,
+		time: new Date().toISOString(),
+		type,
+	};
+	return cloudevent;
+};
+
+module.exports = { create };
