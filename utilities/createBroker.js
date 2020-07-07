2b37b0c1bc9657b7ca859173df644634d11605d5
1Mill <erik.ekberg@outlook.com>
2020-07-06 20:19:13 -0700

    Throw error if missing

--- utilities/createBroker.js
+++ utilities/createBroker.js
@@ -1,7 +1,10 @@
-const publish = require('./publish');
-const subscribe = require('./subscribe');
+const { KAFKA_EVENTTYPE } = require('../lib/constants');
 
 const createBroker = ({ id, eventType, urls = [] }) => {
+	// TODO: Support more event types (e.g. rabbitmq)
+	if (eventType !== KAFKA_EVENTTYPE) {
+		throw Error('Unsupported broker event type');
+	}
 	return { id, eventType, urls };
 };
 
