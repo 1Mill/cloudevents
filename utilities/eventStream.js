d36eac8804b7b6fed03a783fe1617802864bbd35
1Mill <erik.ekberg@outlook.com>
2020-08-25 20:42:16 -0700

    Update eventStream.js

--- utilities/eventStream.js
+++ utilities/eventStream.js
@@ -1,10 +1,25 @@
+const { Kafka } = require('kafkajs');
+const { createAuthentication } = require('./createAuthentication');
+
 const eventStream = async ({
+	id,
 	mechanism,
 	password,
 	protocal,
 	urls,
 	username,
 }) => {
+	const authentication = createAuthentication({
+		mechanism,
+		password,
+		username,
+	})
+	const kafka = new Kafka({
+		...authentication,
+		brokers: urls,
+		clientId: id,
+	})
+
 	const emit = ({}) => { console.log('Emitting') }
 	const listen = ({}) => { console.log('Listening') }
 	return { emit, listen }
