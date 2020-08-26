2d3f94cf1c07ceedaf42152ee2fdaed8c9fe38fd
1Mill <erik.ekberg@outlook.com>
2020-08-25 20:33:01 -0700

    Update eventStream.js

--- utilities/eventStream.js
+++ utilities/eventStream.js
@@ -1,47 +1,12 @@
-const { Kafka } = require('kafkajs');
-
-class EventStream {
-	constructor({ id, mechanism, password, protocal, urls, username }) {
-		this.id = id
-		this.mechanism = mechanism
-		this.password = password
-		this.protocal = protocal
-		this.urls = urls
-		this.username = username
-	}
-
-	_authentication() {
-		if (this.mechanism === 'sasl') {
-			return {
-				sasl: {
-					password: this.password,
-					username: this.username,
-				},
-				ssl: true,
-			}
-		}
-		if (this.mechanism === 'ssl') {
-			return {
-				password: this.password,
-				username: this.username,
-			}
-		}
-		return {}
-	}
-
-	_kafka() {
-		return new Kafka({
-			...this._authentication,
-			brokers: this.urls,
-			clientId: this.id,
-		})
-	}
-
-	async emit({ cloudevent }) {
-	}
-
-	async listen({ types, handler }) {
-	}
+const eventStream = async ({
+	mechanism,
+	password,
+	protocal,
+	protocal,
+	urls,
+	username,
+}) => {
+	console.log('testing');
 }
 
-module.exports = { EventStream };
+module.exports = { eventStream }
