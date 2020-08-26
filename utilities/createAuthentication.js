0311d2a80dd42b7e63de52edd980972d4da83e15
1Mill <erik.ekberg@outlook.com>
2020-08-25 20:41:08 -0700

    Update createAuthentication.js

--- utilities/createAuthentication.js
+++ utilities/createAuthentication.js
@@ -1,8 +1,19 @@
 // Client Configuration for KafkaJS: https://kafka.js.org/docs/configuration
-const createAuthentication = ({ type, config }) => {
-	if (type === 'ssl') { return config; }
-	if (type === 'sasl') { return { sasl: config, ssl: true }; }
-	return {};
+const createAuthentication = ({
+	mechanism,
+	password,
+	username,
+}) => {
+	if (mechanism === 'sasl') {
+		return {
+			sasl: { password, username },
+			ssl: true,
+		}
+	}
+	if (mechanism === 'ssl') {
+		return { password, username }
+	}
+	return {}
 }
 
 module.exports = { createAuthentication };
