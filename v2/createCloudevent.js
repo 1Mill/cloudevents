18752b92f97b10676627d610c3150bd1fae778e5
1Mill <erik.ekberg@outlook.com>
2020-09-13 13:33:28 -0700

    Refactor cloudevent interface

--- v2/createCloudevent.js
+++ v2/createCloudevent.js
@@ -1,6 +1,6 @@
 const createCloudevent = ({
-	data = null,
-	datacontenttype = 'application/json',
+	data = undefined,
+	datacontenttype = undefined,
 	dlx = 'dlx',
 	enrichment = undefined,
 	enrichmentcontenttype = undefined,
@@ -9,19 +9,42 @@ const createCloudevent = ({
 	specversion = '1.0',
 	type,
 }) => {
+	if (!id) { throw new Error('Cloudevent "id" is as required') }
+	if (!source) { throw new Error('Cloudevent "source" is as required') }
+	if (!type) { throw new Error('Cloudevent "type" is as required') }
+
+	const isDataEncoded = data !== undefined && datacontenttype !== undefined
+	const isEnrichmentEncoded = enrichment !== undefined && enrichmentcontenttype !== undefined
+
 	const cloudevent = {
-		data: JSON.stringify(data),
-		datacontenttype,
+		// Required defaults
 		dlx,
-		enrichment,
-		enrichmentcontenttype,
 		id,
 		source,
 		specversion,
 		time: new Date().toISOString(),
 		type,
-	};
-	return cloudevent;
-};
 
-module.exports = { createCloudevent };
+		// Optional original data
+		data: isDataEncoded
+			? data
+			: JSON.stringify(data),
+		datacontenttype: isDataEncoded
+			? datacontenttype
+			: 'application/json',
+
+		// Optional enrichment data
+		enrichmentdata: isEnrichmentEncoded
+			? enrichment
+			: JSON.stringify(enrichment),
+		enrichmentdatacontenttype: isEnrichmentEncoded
+			? enrichmentcontenttype
+			: 'application/json',
+		enrichmenttime: enrichment === undefined
+			? undefined
+			: new Date().toISOString(),
+	}
+	return cloudevent
+}
+
+module.exports = { createCloudevent }
