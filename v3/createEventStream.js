e90677a963c86ebe590bf4937a46513e7e11bc9d
1Mill <erik.ekberg@outlook.com>
2020-09-13 13:59:17 -0700

    Alias kafkaCreateInstance

--- v3/createEventStream.js
+++ v3/createEventStream.js
@@ -1,5 +1,5 @@
-const { createInstance } = require('./kafka')
 const { PROTOCOL_KAFKA, PROTOCOL_LAMBDA } = require('./lib/constants')
+const { createInstance: kafkaCreateInstance } = require('./kafka')
 
 const SUPPORTED_PROTOCOLS = [
 	PROTOCOL_KAFKA,
@@ -17,7 +17,7 @@ const createEventStream = ({
 	if (!SUPPORTED_PROTOCOLS.includes(protocol)) { throw new Error(`The "${protocol}" protocol is not supported`);}
 
 	if (protocol === PROTOCOL_KAFKA) {
-		const { emit, listen } = createInstance({
+		const { emit, listen } = kafkaCreateInstance({
 			id,
 			mechanism,
 			password,
