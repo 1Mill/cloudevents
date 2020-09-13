f82e8899fc24ff82e3e7172523ed54fb30fd4713
1Mill <erik.ekberg@outlook.com>
2020-09-13 13:42:54 -0700

    Update export

--- v3/index.js
+++ v3/index.js
@@ -3,11 +3,11 @@ const { createEventStream } = require('./createEventStream')
 const { enrichCloudevent } = require('./enrichCloudevent')
 const { isEnriched } = require('./isEnriched')
 
-const v2 = {
+const v3 = {
 	createCloudevent,
 	createEventStream,
 	enrichCloudevent,
 	isEnriched,
 }
 
-module.exports = { v2 }
+module.exports = { v3 }
