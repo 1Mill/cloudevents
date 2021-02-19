3db22c7fbc5fd74936669b286efcce539d689329
erik-ekberg-sg <erik@singlegrain.com>
2021-02-19 14:59:02 -0800

    Export v5 from v5 namespace

--- v5/index.js
+++ v5/index.js
@@ -2,11 +2,11 @@ const { PROTOCOL_KAFKA, PROTOCOL_LAMBDA } = require('./lib/constants')
 const { createCloudevent } = require('./createCloudevent')
 const { createEventStream } = require('./createEventStream')
 
-const v4 = {
+const v5 = {
 	PROTOCOL_KAFKA,
 	PROTOCOL_LAMBDA,
 	createCloudevent,
 	createEventStream,
 }
 
-module.exports = { v4 }
+module.exports = { v5 }
