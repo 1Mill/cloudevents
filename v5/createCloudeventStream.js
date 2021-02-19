30004369d63f9ef6edac3cef3c2af2bc4f7ff7f3
erik-ekberg-sg <erik@singlegrain.com>
2021-02-19 15:13:47 -0800

    Add defaults

--- v5/createCloudeventStream.js
+++ v5/createCloudeventStream.js
@@ -7,18 +7,24 @@ const SUPPORTED_PROTOCOLS = [
 	PROTOCOL_LAMBDA,
 ]
 
+const _throwRequired = (attribute) => {
+	throw new Error(`The ${attribute} argument is required`)
+}
+
 const createCloudeventStream = ({
-	id,
-	mechanism,
-	password,
-	protocol,
-	urls,
-	username,
+	id = process.env.CLOUDEVENT_STREAM_ID,
+	mechanism = process.env.CLOUDEVENT_STREAM_MECHANISM,
+	password = process.env.CLOUDEVENT_STREAM_PASSWORD,
+	protocol = process.env.CLOUDEVENT_STREAM_PROTOCOL,
+	urls = (process.env.CLOUDEVENT_STREAM_URLS || '').split(','),
+	username = process.env.CLOUDEVENT_STREAM_USERNAME,
 }) => {
-	if (!protocol) { throw new Error('The "protocol" argument is required') }
+	if (!protocol) { _throwRequired('protocol') }
 	if (!SUPPORTED_PROTOCOLS.includes(protocol)) { throw new Error(`The "${protocol}" protocol is not supported`) }
 
 	if (protocol === PROTOCOL_KAFKA) {
+		if (!id) { _throwRequired('id') }
+		if (!urls.length === 0) { _throwRequired('urls') }
 		return kafkaCreateInstance({
 			id,
 			mechanism,
