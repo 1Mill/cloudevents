2207373ba7c24a888147b12a6d23c9dcb44503d8
erik-ekberg-sg <erik@singlegrain.com>
2021-02-19 14:39:28 -0800

    Abstract v5

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const { v3 } = require('./v3')
 const { v4 } = require('./v4')
+const { v5 } = require('./v5')
 
-module.exports = Object.freeze({ v3, v4 })
+module.exports = Object.freeze({ v3, v4, v5 })
