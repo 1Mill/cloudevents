345a1072dc0d0c97cd0b2053dea7b6097f59b851
1Mill <erik.ekberg@outlook.com>
2022-09-16 15:46:13 -0700

    Formatting

--- src/index.js
+++ src/index.js
@@ -22,53 +22,117 @@ export class Cloudevent {
 		// *******
 		// * Required fields by Cloudevent v1 specification
 		const idValue = nanoid(fetchNodeEnv('MILL_CLOUDEVENTS_NANOID_LENGTH', 21))
-		setAttribute({ cloudevent, name: 'id', types: ['string'], value: idValue })
+		setAttribute({
+			cloudevent,
+			name: 'id',
+			types: ['string'],
+			value: idValue
+		})
 
 		const sourceValue = source || fetchNodeEnv('MILL_CLOUDEVENTS_SOURCE')
-		setAttribute({ cloudevent, name: 'source', types: ['string'], value: sourceValue })
+		setAttribute({
+			cloudevent,
+			name: 'source',
+			types: ['string'],
+			value: sourceValue
+		})
 
 		const typeValue = type
-		setAttribute({ cloudevent, name: 'type', types: ['string'], value: typeValue })
+		setAttribute({
+			cloudevent,
+			name: 'type',
+			types: ['string'],
+			value: typeValue
+		})
 
 		const specversionValue = specversion || '1.0'
-		setAttribute({ cloudevent, name: 'specversion', types: ['string'], value: specversionValue })
+		setAttribute({
+			cloudevent,
+			name: 'specversion',
+			types: ['string'],
+			value: specversionValue
+		})
 
 		const timeValue = new Date().toISOString()
-		setAttribute({ cloudevent, name: 'time', types: ['string'], value: timeValue })
+		setAttribute({
+			cloudevent,
+			name: 'time',
+			types: ['string'],
+			value: timeValue
+		})
 		// *******
 
 		// *******
 		// * Optional fields by Cloudevent v1 specification
-		setAttribute({ cloudevent, name: 'data', value: data })
+		setAttribute({
+			cloudevent,
+			name: 'data',
+			value: data
+		})
 
 		const datacontenttypeValue = typeof data !== 'undefined'
 			? datacontenttype || 'application/json'
 			: datacontenttype
-		setAttribute({ cloudevent, name: 'datacontenttype', types: ['string', 'undefined'], value: datacontenttypeValue })
+		setAttribute({
+			cloudevent,
+			name: 'datacontenttype',
+			types: ['string', 'undefined'],
+			value: datacontenttypeValue
+		})
 
-		setAttribute({ cloudevent, name: 'dataschema', types: ['string', 'undefined'], value: dataschema })
+		setAttribute({
+			cloudevent,
+			name: 'dataschema',
+			types: ['string', 'undefined'],
+			value: dataschema
+		})
 
-		setAttribute({ cloudevent, name: 'subject', types: ['string', 'undefined'], value: subject })
+		setAttribute({
+			cloudevent,
+			name: 'subject',
+			types: ['string', 'undefined'],
+			value: subject
+		})
 		// *******
 
 		// *******
 		// * Required in-house extentions
 		const origintimeValue = origintime || timeValue
-		setAttribute({ cloudevent, name: 'origintime', types: ['string'], value: origintimeValue })
+		setAttribute({
+			cloudevent,
+			name: 'origintime',
+			types: ['string'],
+			value: origintimeValue
+		})
 
 		const originidValue = originid || idValue
-		setAttribute({ cloudevent, name: 'originid', types: ['string'], value: originidValue })
+		setAttribute({
+			cloudevent,
+			name: 'originid',
+			types: ['string'],
+			value: originidValue
+		})
 
 		const originsourceValue = originsource || sourceValue
 		setAttribute({ cloudevent, name: 'originsource', types: ['string'], value: originsourceValue })
 
 		const origintypeValue = origintype || typeValue
-		setAttribute({ cloudevent, name: 'origintype', types: ['string'], value: origintypeValue })
+		setAttribute({
+			cloudevent,
+			name: 'origintype',
+			types: ['string'],
+			value: origintypeValue
+		})
 		// *******
 
 		// *******
 		// * Optional in-house extentions
-		setAttribute({ cloudevent, name: 'originatorid', types: ['string', 'undefined'], value: originatorid })
+		setAttribute({
+			cloudevent,
+			name: 'originatorid',
+			types: ['string', 'undefined'],
+			value: originatorid
+		})
 		// *******
 	}
 }
