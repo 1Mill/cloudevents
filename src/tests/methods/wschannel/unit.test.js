4df0ac7fb0ad9f81a2cacce814a459f23a9c835a
1Mill <erik.ekberg@outlook.com>
2022-09-20 21:23:58 -0700

    Update unit.test.js

--- src/tests/methods/wschannel/unit.test.js
+++ src/tests/methods/wschannel/unit.test.js
@@ -4,10 +4,7 @@ import { expect } from 'chai'
 describe('cloudevent.wschannel', () => {
 	it('returns itself', () => {
 		const ce = new Cloudevent({ source: 'some-source', type: 'some-type' })
-		const origin = new Cloudevent({ source: 'source-of-origin', type: 'type-of-origin' })
-
-		const result = ce.wschannel({ cloudevent: origin })
-
+		const result = ce.wschannel({ wschannelid: 'some-wschannel-id' })
 		expect(result).to.eq(ce)
 	})
 })
